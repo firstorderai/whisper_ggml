211acb80586f218f458a1f2d67ebdfde013517b8
kenzo-ai <kenzo@firstorder.ai>
2025-06-15 17:37:25 +0800

    fix compile error

--- ios/Classes/include/dart_api_dl_custom.h
+++ ios/Classes/include/dart_api_dl_custom.h
@@ -26,7 +26,7 @@ typedef void* DartApiEntry_function;
 DartApiEntry_function FindFunctionPointer(const DartApiEntry* entries,
                                           const char* name) {
     while (entries->name != NULL) {
-        if (strcmp(entries->name, name) == 0) return entries->function;
+        if (strcmp(entries->name, name) == 0) return (DartApiEntry_function)entries->function;
         entries++;
     }
     return NULL;
